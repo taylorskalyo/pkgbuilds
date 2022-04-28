19e1be572609d5273d785cd3ab204355c9a5d2dc
taylorskalyo <taylorskalyo@users.noreply.github.com>
2022-04-27 22:22:31 -0400

    Prevent PKGBUILD being interpretted as shell script

--- dotfiles/nvim/plugins.lua
+++ dotfiles/nvim/plugins.lua
@@ -158,6 +158,9 @@ return require("packer").startup(function(use)
     "jose-elias-alvarez/null-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
+      -- Prevent PKGBUILD being interpretted as shell script.
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/587
+      vim.cmd("au BufNewFile,BufRead PKGBUILD set filetype=PKGBUILD")
       require("null-ls").setup({
         sources = {
           require("null-ls").builtins.formatting.stylua,
