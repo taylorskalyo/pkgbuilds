c77d56ee4974962b62a1a3a4fc026977c0eda764
taylorskalyo <taylorskalyo@users.noreply.github.com>
2022-05-19 21:37:56 -0400

    Use full block for nvim indents

--- dotfiles/nvim/plugins-indent-blankline.lua
+++ dotfiles/nvim/plugins-indent-blankline.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require("indent_blankline").setup {
       char = '│',
-      context_char = '┃',
+      context_char = '█',
       show_current_context = true,
       show_trailing_blankline_indent = false,
     }
