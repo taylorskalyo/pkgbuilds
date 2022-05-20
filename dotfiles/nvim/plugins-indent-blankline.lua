87889cfd90b77840bda3439122d87382cd299c83
taylorskalyo <taylorskalyo@users.noreply.github.com>
2022-05-19 21:37:02 -0400

    Remove unneeded nvim indent filetype exclusions

--- dotfiles/nvim/plugins-indent-blankline.lua
+++ dotfiles/nvim/plugins-indent-blankline.lua
@@ -6,15 +6,6 @@ return {
       context_char = '┃',
       show_current_context = true,
       show_trailing_blankline_indent = false,
-      filetype_exclude = {
-        "lspinfo",
-        "packer",
-        "checkhealth",
-        "help",
-        "man",
-        "",
-        "json",
-      },
     }
   end,
 }
