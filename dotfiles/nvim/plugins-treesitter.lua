31670f993bbc965f8f21b6f5af7d61f7cac40db6
taylorskalyo <taylorskalyo@users.noreply.github.com>
2026-08-15 15:26:37 -0500

    fix(nvim): use treesitter for highlighting and indents
    See also: 933526f7369ce81ea36e93ff47d63ca5a2101119

--- dotfiles/nvim/plugins-treesitter.lua
+++ dotfiles/nvim/plugins-treesitter.lua
@@ -8,4 +8,14 @@ return {
       install_dir = vim.fn.stdpath('data') .. '/site'
     }
   end,
+  init = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Enable treesitter highlighting
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
