772ae355e2b558554ae2235fd0ea3535448ebddf
taylorskalyo <taylorskalyo@users.noreply.github.com>
2026-08-28 10:53:37 -0500

    fix(nvim): only enable treesitter indenting if grammar supports it
    For example, the qmljs grammar does not provide indent hints. Telling
    vim to use treesitter for indents in this case results in broken
    indenting behavior. Querying for indent support before enabling
    treesitter indenting fixes this.

--- dotfiles/nvim/plugins-treesitter.lua
+++ dotfiles/nvim/plugins-treesitter.lua
@@ -11,13 +11,14 @@ return {
   init = function()
     vim.api.nvim_create_autocmd('FileType', {
       callback = function(args)
-
-        -- Auto-install languages
         local ft = args.match
         local bufnr = args.buf
+
         local lang = vim.treesitter.language.get_lang(ft)
         local nvim_treesitter = require("nvim-treesitter")
-        if (vim.list_contains(nvim_treesitter.get_available(), lang)) then
+
+        -- Auto-install languages
+        if lang and (vim.list_contains(nvim_treesitter.get_available(), lang)) then
           nvim_treesitter.install(lang):await(function()
             if not vim.api.nvim_buf_is_loaded(bufnr) then
               return
@@ -27,7 +28,9 @@ return {
             vim.treesitter.start(bufnr)
 
             -- Enable treesitter-based indentation
-            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            if vim.treesitter.query.get(lang, "indents") then
+              vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
           end)
         end
       end,
